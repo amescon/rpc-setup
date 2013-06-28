bc5a5d075900182b97f6dc555e6ef5299ab39a6b
Martin <mdk@amescon.com>
2013-06-28 10:23:47 +0200

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,4 @@
+# download and install raspicomm module per one-liner
+# rpc_rs485_install_driver.sh is uploaded to downloads.amescon.com
+
+wget http://downloads.amescon.com/rpc_rs485_install_driver.sh && sudo ./rpc_rs485_install_driver.sh
